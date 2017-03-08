8f0272b3112e9bae04103acfdad5428865c6c028
Sehra <Sehra@openroam-prg-gm-1-131-41.net.univ-paris-diderot.fr>
2017-03-08 15:35:33 +0100

    Third Commit

--- src/MainTP1/DictConstructor.java
+++ src/MainTP1/DictConstructor.java
@@ -4,6 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -18,21 +20,40 @@ public class DictConstructor {
 	public void getWikiWords() throws IOException
 	{
 		Document doc = Jsoup.connect(wikiURL).get();
-        Elements tableElements = doc.select("table");        
-        Elements tableRowElements = tableElements.select(":not(thead) tr");
-        File dictWords = new File("src/MainTP1Files/dictWIKI.words");
+		Elements tableElements = doc.select("table");        
+		Elements tableRowElements = tableElements.select(":not(thead) tr");
+		Set<String> dictArray = new HashSet<String>();
+		File dictWords = new File("src/MainTP1Files/dictWIKI.words");
 		FileWriter fw = new FileWriter(dictWords.getAbsoluteFile());
 		BufferedWriter bw = new BufferedWriter(fw);
         
-        for (int i = 0; i < tableRowElements.size(); i++) 
+        for (int i = 1; i < tableRowElements.size(); i++) 
         {
         	Element row = tableRowElements.get(i);
-        	if (i != 0 && row.select("td > a").text().toLowerCase().length() >= 5) 
+        	
+        	if (row.select("td > a").text().toLowerCase().length() >= 5) 
         	{
-        		bw.write(row.select("td > a").text().toLowerCase().replace("é", "e").replace("è", "e").replace("ê", "e").replace("à", "a"));
-        		bw.write("\n");
+        		dictArray.add(row.select("td > a").text().toLowerCase().replace("é", "e").replace("è", "e").replace("ê", "e").replace("à", "a").replace("â", "a"));
         	}
         }
+        
+        String[] dictArrayTemp = new String[dictArray.size()];
+        
+        int i = 0;
+        for (String word : dictArray) 
+        {
+        	dictArrayTemp[i] = word;
+        	i++;
+		}
+                
+        dictArray.clear();
+        java.util.Arrays.sort(dictArrayTemp);
+        
+        for (int j = 0; j < dictArrayTemp.length; j++) 
+        {
+    		bw.write(dictArrayTemp[j]);
+    		bw.write("\n");
+		}
         
         bw.close();
 	}
